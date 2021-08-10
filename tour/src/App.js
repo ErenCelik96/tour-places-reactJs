3aa9a4dd0e599ed20ba555a81d231c5cb40f0fda
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-08-11 00:09:45 +0300

    heading kısmı eklendi

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Header/>
     </div>
   );
 }
---
+++ tour/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import "../header/Header.css";
+
+export const Header = () => {
+    return (
+        <div className="headerContainer">
+            <h1>popular tour places</h1>
+        </div>
+    )
+}
+
+export default Header;
